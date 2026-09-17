82896a353ec91f8940d6cf1e61d2c0e50ab9c6ac
thebharatdev <hiibbhere@gmail.com>
2026-09-17 11:57:51 +0000

    Clear repository

--- src/components/site/Orbs.tsx
+++
@@ -1,9 +0,0 @@
-export function Orbs() {
-  return (
-    <div className="ambient-orbs-wrap" aria-hidden="true">
-      <div className="orb orb-1" />
-      <div className="orb orb-2" />
-      <div className="orb orb-3" />
-    </div>
-  );
-}
